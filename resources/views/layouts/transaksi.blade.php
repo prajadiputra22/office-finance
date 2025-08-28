f7d3c0bfcb5a7143273fd561ca96439da7830d33
prajadiputra22 <darmawan.prajadiputra_ti22@nusaputra.ac.id>
2025-08-28 10:03:16 +0700

    kategori&transaksi

--- resources/views/layouts/transaksi.blade.php
+++ resources/views/layouts/transaksi.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Office Finance')</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite('resources/css/app.css')
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 <body class="font-[Poppins] bg-gray-100">
